a87c34f14e61a6ff9dd68741548fc3b43608e673
firas hamila <firas.hm.1996@gmail.com>
2024-02-23 09:09:29 +0100

    bv

--- Components/ItemGal.js
+++ Components/ItemGal.js
@@ -1,10 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
 const ItemGal = ({ name }) => {
   return (
     <View style={styles.item}>
       <Text style={styles.itemTXT}>{name}</Text>
+      <Ionicons name="trash" size={28} color="red" />
     </View>
   );
 };
@@ -16,7 +18,9 @@ const styles = StyleSheet.create({
     backgroundColor: "#34e4f7",
     marginVertical: 8,
     height: 42,
-    justifyContent: "center",
+    justifyContent: "space-between",
+    flexDirection: "row",
+    alignItems: "center",
     padding: 8,
     borderRadius: 8,
   },
